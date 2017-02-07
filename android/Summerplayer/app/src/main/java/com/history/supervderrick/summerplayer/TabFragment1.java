a8d06715cb0f7db44790b1da04d963b26e9e7616
derrick <derrick.kang@nexstreaming.com>
2017-02-07 16:47:16 +0900

    improved android version

---
+++ android/Summerplayer/app/src/main/java/com/history/supervderrick/summerplayer/TabFragment1.java
@@ -0,0 +1,9 @@
+package com.history.supervderrick.summerplayer;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by derrick on 2017. 2. 7..
+ */
+public class TabFragment1 extends Fragment {
+}
