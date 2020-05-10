7e458a947cf44e3ba9cc76c50d91289533398d80
Derrick <kang.derrick@gmail.com>
2020-05-10 15:31:37 +0900

    reset

--- android/Summerplayer/app/src/main/java/com/history/supervderrick/summerplayer/TabFragment1.java
+++
@@ -1,9 +0,0 @@
-package com.history.supervderrick.summerplayer;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by derrick on 2017. 2. 7..
- */
-public class TabFragment1 extends Fragment {
-}
--- iOS/summerPlayer/Pods/Target Support Files/Pods-summerPlayer/Pods-summerPlayer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_summerPlayer : NSObject
-@end
-@implementation PodsDummy_Pods_summerPlayer
-@end
