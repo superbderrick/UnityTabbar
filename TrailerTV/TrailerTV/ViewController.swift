fa2b80423a69d82e01fb4838b5b0f9f2321a53a4
Derrick <kang.derrick@gmail.com>
2020-05-10 15:33:20 +0900

    added new projec t

---
+++ TrailerTV/TrailerTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrailerTV
+//
+//  Created by derrick on 2020/05/10.
+//  Copyright © 2020 Derrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
