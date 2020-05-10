10ff53426d24181b064c0ca7bb77bc9d89ab1ba6
Derrick <kang.derrick@gmail.com>
2020-05-10 16:17:45 +0900

    added  login controller with background effect

---
+++ TrailerTV/Views/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  TrailerTV
+//
+//  Created by derrick on 2020/05/10.
+//  Copyright © 2020 Derrick. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primaryDark = UIColor(named: "PrimaryDark")!
+    static let primaryLight = UIColor(named: "PrimaryLight")!
+    static let gradientTop = UIColor(named: "GradientTop")!
+    static let gradientBottom = UIColor(named: "GradientBottom")!
+}
---
+++ TrailerTV/Views/GradientView.swift
@@ -0,0 +1,33 @@
+//
+//  GradientView.swift
+//  TrailerTV
+//
+//  Created by derrick on 2020/05/10.
+//  Copyright © 2020 Derrick. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    
+    var gradientLayer : CAGradientLayer!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        gradientLayer = CAGradientLayer()
+        let colorTop = UIColor.gradientTop.cgColor
+        let colorBottom = UIColor.gradientBottom.cgColor
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = frame
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        
+        gradientLayer.frame = frame
+    }
+
+}
